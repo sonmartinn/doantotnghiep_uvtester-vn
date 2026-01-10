20fb97bb6d72a34b9f9b122eacb931d1291a40f3
Le Trung Son <sonmartin2003@gmail.com>
2026-01-10 21:10:41 +0700

    build: fix database.types to database-types imports

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,5 @@
 import { createBrowserClient } from '@supabase/ssr'
-import { type Database } from '@/app/_services/database.types'
+import { type Database } from '@/app/_services/database-types'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
